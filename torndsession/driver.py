ddf9f15e1448e146b11f15446f35320beee7ab8e
Jayme <kalkehcoisa@gmail.com>
2015-06-19 21:56:12 -0300

    Now the driver setting accepts the class of the session handler to use.

--- torndsession/driver.py
+++ torndsession/driver.py
@@ -4,7 +4,6 @@
 # Copyright @ 2014 Mitchell Chu
 
 from __future__ import absolute_import, division, print_function, with_statement
-import sys
 
 
 class SessionDriver(object):
@@ -26,6 +25,7 @@ class SessionDriver(object):
     def remove_expires(self):
         raise NotImplementedError()
 
+
 class SessionDriverFactory(object):
     '''
     session driver factory
@@ -33,14 +33,18 @@ class SessionDriverFactory(object):
     '''
     @staticmethod
     def create_driver(driver, **setings):
+        if issubclass(driver, SessionDriver):
+            return driver
+
         module_name = 'torndsession.%ssession' % driver.lower()
         module = __import__(module_name, globals(), locals(), ['object'])
-        # must use this form.
-        # use __import__('torndsession.' + driver.lower()) just load torndsession.__init__.pyc
+
         cls = getattr(module, '%sSession' % driver.capitalize())
         if not 'SessionDriver' in [base.__name__ for base in cls.__bases__]:
-            raise InvalidSessionDriverException('%s not found in current driver implements ' % driver)
-        return cls              # just return driver class. 
+            raise InvalidSessionDriverException(
+                '%s not found in current driver implements ' % driver)
+        return cls
+
 
 class InvalidSessionDriverException(Exception):
     pass
